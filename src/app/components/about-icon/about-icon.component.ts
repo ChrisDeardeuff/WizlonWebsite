56fdf73c564e920c38cb5dfa021cf2211801dd44
ChrisDeardeuff <chris.deardeuff1131@gmail.com>
2021-06-29 19:24:18 -0400

    added about us/contact page with privacy policy

---
+++ src/app/components/about-icon/about-icon.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {MatIconRegistry} from "@angular/material/icon";
+import { DomSanitizer } from '@angular/platform-browser';
+
+const INFO_ICON = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF">
+<path d="M0 0h24v24H0V0z" fill="none"/>
+<path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
+</svg>`;
+
+@Component({
+  selector: 'app-about-icon',
+  templateUrl: './about-icon.component.html',
+  styleUrls: ['./about-icon.component.css']
+})
+export class AboutIconComponent implements OnInit {
+
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIconLiteral('info', sanitizer.bypassSecurityTrustHtml(INFO_ICON));
+  }
+
+  ngOnInit(): void {
+  }
+
+}
